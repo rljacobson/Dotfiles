2cb52193bbddaf0d42e6772fa6218da64552af7b
Robert Jacobson <rjacobson@halcyon.ai>
2021-12-27 21:38:13 -0500

    Use conda for virtual environments.

--- .config/yadm/ohmyzsh.sh
+++ .config/yadm/ohmyzsh.sh
@@ -3,8 +3,14 @@
 # oh-my-ZSH, from https://github.com/ohmyzsh/ohmyzsh#via-curl
 printHeading "Installing Oh-My-ZSH"
 printDivider
+
 # This script requires git.
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 # The theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# ZSH Auto-Suggestions Plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 printDivider
